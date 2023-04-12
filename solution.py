d0092c71865c496ae3072a5fdfd19b0c8a5ee66e
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-12 20:23:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ from hyppo.ksample import MMD
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = MMD(compute_kernel='rbf', gamma=1).test(x, y])[1]
+    p_value = MMD(compute_kernel='rbf', gamma=1).test(x, y)[1]
     return p_value < 0.09
