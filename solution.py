e573db8f40141b61544ad2b9cfd8412cd7062116
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-12 20:12:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from hyppo.ksample import MMD
 
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = ks_2samp(x, y).pvalue
+    p_value = MMD(compute_kernel='rbf', gamma=1).test(x, y])[1]
     return p_value < 0.09
