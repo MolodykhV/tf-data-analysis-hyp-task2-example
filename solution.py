7a43c31d33d83452e1b9307cd1e1e53b55d5ea72
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-13 18:37:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import MMD
+from scipy.stats import chi2
 
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = MMD(compute_kernel='rbf', gamma=1).test(x, y)[1]
-    return p_value < 0.09
+    n = len(x)
+    dof = n - 1
+    chi_stat = sum([(o - e)**2.0 / e for o,e in zip(y, x)])
+    return chi_stat >= chi2.ppf(q=1-0.09, df=dof)
